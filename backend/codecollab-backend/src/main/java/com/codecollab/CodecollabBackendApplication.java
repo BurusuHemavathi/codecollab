11ad8eba73faeb912f0157a359455455c1350523
Burusu Hemavathi <hemahemavathi321@gmail.com>
2026-06-13 20:02:19 +0530

    Initial project structure

---
+++ backend/codecollab-backend/src/main/java/com/codecollab/CodecollabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecollabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecollabBackendApplication.class, args);
+    }
+
+}
---
+++ backend/codecollab-backend/src/main/java/com/codecollab/entity/User.java
@@ -0,0 +1,30 @@
+package com.codecollab.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
