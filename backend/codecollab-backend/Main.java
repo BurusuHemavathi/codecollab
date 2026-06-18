b71328ded307cde6fc4a107f1c48f21dd6bc8e8e
Burusu Hemavathi <hemahemavathi321@gmail.com>
2026-06-18 16:12:10 +0530

    Implemented Java code execution

---
+++ backend/codecollab-backend/Main.java
@@ -0,0 +1,1 @@
+public class Main { public static void main(String[] args){ int a=10/0; }}
--- backend/codecollab-backend/src/main/java/com/codecollab/service/CodeExecutionServiceImpl.java
+++ backend/codecollab-backend/src/main/java/com/codecollab/service/CodeExecutionServiceImpl.java
@@ -2,6 +2,8 @@ package com.codecollab.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+
 @Service
 public class CodeExecutionServiceImpl
         implements CodeExecutionService {
@@ -9,6 +11,71 @@ public class CodeExecutionServiceImpl
     @Override
     public String executeJavaCode(String code) {
 
-        return "Execution Module Coming Next";
+        try {
+
+            File file = new File("Main.java");
+
+            FileWriter writer =
+                    new FileWriter(file);
+
+            writer.write(code);
+            writer.close();
+
+            Process compileProcess =
+                    Runtime.getRuntime()
+                            .exec("javac Main.java");
+
+            compileProcess.waitFor();
+
+            BufferedReader compileError =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    compileProcess.getErrorStream()
+                            )
+                    );
+
+            StringBuilder errorOutput =
+                    new StringBuilder();
+
+            String line;
+
+            while ((line = compileError.readLine())
+                    != null) {
+
+                errorOutput.append(line)
+                        .append("\n");
+            }
+
+            if (!errorOutput.isEmpty()) {
+                return errorOutput.toString();
+            }
+
+            Process runProcess =
+                    Runtime.getRuntime()
+                            .exec("java Main");
+
+            BufferedReader outputReader =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    runProcess.getInputStream()
+                            )
+                    );
+
+            StringBuilder output =
+                    new StringBuilder();
+
+            while ((line = outputReader.readLine())
+                    != null) {
+
+                output.append(line)
+                        .append("\n");
+            }
+
+            return output.toString();
+
+        } catch (Exception e) {
+
+            return e.getMessage();
+        }
     }
 }
